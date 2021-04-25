870c68c776ae0b89dec01d136b7558e711c3e766
Bartosz Kończyk <bartkon@gmail.com>
2021-04-25 10:22:24 +0200

    commit

--- src/main/java/pl/sda/spring/thymeleafexample/domain/starter/StarterController.java
+++ src/main/java/pl/sda/spring/thymeleafexample/domain/starter/StarterController.java
@@ -1,15 +1,27 @@
 package pl.sda.spring.thymeleafexample.domain.starter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class StarterController {
 
-    @GetMapping("/starter")
-    public String viewStarter(final ModelMap modelMap) {
-        modelMap.addAttribute("helloMsg", "WHATEVER");
+    @GetMapping("/starter/{name}")
+    public String viewStarter(@PathVariable String name, final Model model) {
+        model
+            .addAttribute("name", name)
+            .addAttribute("helloMsg", "Hello");
         return "starter";
     }
+
+//        @GetMapping("/starter/{name}")
+//    public String viewStarter(@PathVariable String name, final ModelMap modelMap) {
+//        modelMap
+//            .addAttribute("name", name)
+//            .addAttribute("helloMsg", "Hello");
+//        return "starter";
+//    }
 }
