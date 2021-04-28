6518df088fc035ca2cbb414f5c4edfe7a8e8fd10
Bartosz Kończyk <bartkon@gmail.com>
2021-04-28 22:49:30 +0200

    fixed

--- src/main/java/pl/sda/spring/thymeleafexample/domain/employee/Employee.java
+++ src/main/java/pl/sda/spring/thymeleafexample/domain/employee/Employee.java
@@ -2,11 +2,14 @@ package pl.sda.spring.thymeleafexample.domain.employee;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -18,8 +21,13 @@ public class Employee {
     private Long id;
     @NotEmpty(message = "Name should not be empty")
     private String name;
+    @NotEmpty(message = "Surname should not be empty")
     private String surname;
+    @Email(message = "Invalid email")
+    @NotEmpty(message = "E-mail is required")
     private String email;
     private String pesel;
+    @Range(min = 0, max = 150, message = "Age has to be between 0 and 150")
+    @NotNull(message = "Age is required")
     private Integer age;
 }
